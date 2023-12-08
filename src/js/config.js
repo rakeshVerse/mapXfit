5da54d9752a2088d3be059c7db37faac23f82f0b
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-12-08 16:08:49 +0530

    Minor improvements: passing global dependencies to constructor, optimizing measure rendering logic

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,18 @@
 export const LOCAL_STORAGE_KEY = 'workouts';
+export const MAP_ZOOM = 13;
+export const MAP_ZOOM_WORLD = 2.5;
+export const WORKOUT_TYPES = ['running', 'cycling'];
+export const CONFIG = {
+  1: {
+    logo: `🏃🏾`,
+    workout: `Running`,
+    speedUnit: `MIN/KM`,
+    popupClass: `running-popup`,
+  },
+  2: {
+    logo: `🚴‍♀️`,
+    workout: `Cycling`,
+    speedUnit: `KM/H`,
+    popupClass: `cycling-popup`,
+  },
+};
